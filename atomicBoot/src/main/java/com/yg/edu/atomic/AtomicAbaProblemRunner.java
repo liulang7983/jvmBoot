2b76404f43d30e680ebd345fa8a4f3a746073506
ming.li <ming.li@goupwith.com>
2023-03-16 20:18:22 +0800

    提交

---
+++ atomicBoot/src/main/java/com/yg/edu/atomic/AtomicAbaProblemRunner.java
@@ -0,0 +1,56 @@
+package com.yg.edu.atomic;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
+
+
+@Slf4j
+public class AtomicAbaProblemRunner {
+    static AtomicInteger atomicInteger = new AtomicInteger(1);
+    /**
+     此时实际上是出现了ABA问题，在线程main途中，线程other是把atomicInteger从1改到2在改回了1，
+     然后线程main执行atomicInteger.compareAndSet(a,2)才不会失败
+     */
+
+    public static void main(String[] args) {
+        Thread main = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = atomicInteger.get();
+                log.info("操作线程"+Thread.currentThread().getName()+"--修改前操作数值:"+a);
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                boolean isCasSuccess = atomicInteger.compareAndSet(a,2);
+                if(isCasSuccess){
+                    log.info("操作线程"+Thread.currentThread().getName()+"--Cas修改后操作数值:"+atomicInteger.get());
+                }else{
+                    log.info("CAS修改失败");
+                }
+            }
+        },"主线程");
+
+        Thread other = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atomicInteger.incrementAndGet();// 1+1 = 2;
+                log.info("操作线程"+Thread.currentThread().getName()+"--increase后值:"+atomicInteger.get());
+                atomicInteger.decrementAndGet();// atomic-1 = 2-1;
+                log.info("操作线程"+Thread.currentThread().getName()+"--decrease后值:"+atomicInteger.get());
+            }
+        },"干扰线程");
+
+        main.start();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        other.start();
+
+    }
+}
