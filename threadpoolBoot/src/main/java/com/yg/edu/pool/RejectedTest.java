4716d6b48bba37379300a02f85de1c69c0b7baf8
ming.li <ming.li@goupwith.com>
2023-03-20 14:37:58 +0800

    提交

---
+++ threadpoolBoot/src/main/java/com/yg/edu/pool/RejectedTest.java
@@ -0,0 +1,38 @@
+package com.yg.edu.pool;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author ming.li
+ * @date 2023/3/20 11:25
+ */
+public class RejectedTest {
+    public static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 4, 100, TimeUnit.SECONDS, new LinkedBlockingDeque<>(200), new RejectedTest.RejectedCustom());
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+            ThreadService threadService = new ThreadService(i, Thread.currentThread().getName());
+            //此时如果用submit，否则下面的拒绝策越里的r不是ThreadService对象，是一个调用了newTaskFor()方法封装后的对象
+            //只能用execute
+            threadPoolExecutor.execute(threadService);
+        }
+    }
+
+    public static class RejectedCustom implements RejectedExecutionHandler {
+
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+
+            String s = JSON.toJSONString(r);
+            if (r instanceof ThreadService) {
+                System.out.println("我是threadService");
+            }
+            System.out.println("我要把:" + s + "写入到数据库,晚上定时调度再执行");
+        }
+    }
+}
--- threadpoolBoot/src/main/java/com/yg/edu/pool/ThreadPool3.java
+++ threadpoolBoot/src/main/java/com/yg/edu/pool/ThreadPool3.java
@@ -11,11 +11,11 @@ import java.util.concurrent.TimeUnit;
  * @slogan: 天下风云出我辈，一入代码岁月催
  * @description:
  **/
-public class ThreadPool2 {
+public class ThreadPool3 {
 
     public static void main(String[] args)throws Exception {
 
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 10, 5000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5));
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1000, 1000, 5000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5));
         threadPoolExecutor.allowCoreThreadTimeOut(true);
         //此时停止其实停不掉，看源码可得，核心池满之前是不判断状态的，只有在添加队列的时候才会怕段状态   ! isRunning(recheck)
         //所以所谓的shutdown()后新加入的线程不会执行只是指的是队列里的
@@ -28,7 +28,7 @@ public class ThreadPool2 {
             },i);
         }
 
-        threadPoolExecutor.shutdown();  //running->shutdown
+        threadPoolExecutor.shutdownNow();  //running->stop
         System.out.println("我执行了停止");
 
 
