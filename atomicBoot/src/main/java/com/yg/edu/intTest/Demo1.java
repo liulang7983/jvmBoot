b3acc29efb1e460823b2582c735e4858ab38b07b
ming.li <ming.li@goupwith.com>
2023-05-16 19:54:59 +0800

    println原子性测试

---
+++ atomicBoot/src/main/java/com/yg/edu/intTest/Demo1.java
@@ -0,0 +1,14 @@
+package com.yg.edu.intTest;
+
+/**
+ * @author ming.li
+ * @date 2023/5/16 17:21
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        Integer a=1;
+        if (a.equals(1)){
+            System.out.println("我是1");
+        }
+    }
+}
---
+++ syncronizedBoot/src/main/java/com/yg/edu/SystemDemo.java
@@ -0,0 +1,50 @@
+package com.yg.edu;
+
+import org.junit.Test;
+
+/**
+ * @author ming.li
+ * @date 2023/5/16 19:46
+ */
+public class SystemDemo {
+
+    private volatile static int counter=0;
+    private static Object o=new Object();
+    //此时不加锁不加volatile修饰，十个线程那么到不了10000(哪怕加了volatile也到不了10000，因为volatile不保证原子性)
+    @Test
+    public void test1(){
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    counter++;
+                }
+            });
+            thread.start();
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(counter);
+    }
+    //此时不加锁不加volatile也可以到10000，是因为println里面是加锁的
+    @Test
+    public void test2(){
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    System.out.println( counter++);;
+                }
+            });
+            thread.start();
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(counter);
+    }
+
+}
