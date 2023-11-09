e7fbf9950465bb74bb68abed7ac554ca2282aef8
ming.li <ming.li@goupwith.com>
2023-11-09 14:29:09 +0800

    callable测试

---
+++ threadpoolBoot/src/main/java/com/yg/edu/callableTest/MyCallable.java
@@ -0,0 +1,14 @@
+package com.yg.edu.callableTest;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author ming.li
+ * @date 2023/11/9 14:27
+ */
+public class MyCallable implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        return "我是MyCallable";
+    }
+}
