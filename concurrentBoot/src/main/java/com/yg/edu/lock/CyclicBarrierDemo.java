5f98b6f3516a7c2080a0badbd7622e1f68393537
ming.li <1430793456@qq.com>
2024-01-08 16:02:59 +0800

    CyclicBarrier处理

--- concurrentBoot/src/main/java/com/yg/edu/lock/CyclicBarrierDemo.java
+++ concurrentBoot/src/main/java/com/yg/edu/lock/CyclicBarrierDemo.java
@@ -16,7 +16,7 @@ public class CyclicBarrierDemo {
                 Thread.sleep(10_00);
                 System.out.println("t1 在准备 ");
                 cyclicBarrier.await();   // 等另外一个一个线程准备好 然后开始做事情
-                System.out.println("t1 准备好了  ");
+                System.out.println("t1开始运行");
             } catch (InterruptedException | BrokenBarrierException e) {
                 e.printStackTrace();
             }
@@ -28,14 +28,14 @@ public class CyclicBarrierDemo {
                 Thread.sleep(5_000);
                 System.out.println("t2 在准备  ");
                 cyclicBarrier.await(); // 等另外一个一个线程准备好 然后开始做事情
-                System.out.println("t2 准备好了  ");
+                System.out.println("t2开始运行");
             } catch (InterruptedException | BrokenBarrierException e) {
                 e.printStackTrace();
             }
         }).start();
         System.out.println(" 裁判 在准备 ");
         cyclicBarrier.await();
-        System.out.println(" 裁判 准备好了 ");
+        System.out.println(" 裁判鸣枪");
         while (true){
             if (cyclicBarrier.getNumberWaiting()==0){
                 System.out.println(" 时间到: 开始比赛");
--- concurrentBoot/src/main/java/com/yg/edu/lock/CyclicBarrierPig.java
+++ concurrentBoot/src/main/java/com/yg/edu/lock/CyclicBarrierPig.java
@@ -1,7 +1,5 @@
 package com.yg.edu.lock;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
