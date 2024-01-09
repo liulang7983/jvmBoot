aa49fd4da523b463662761885c55883d1ff0a0d1
ming.li <1430793456@qq.com>
2024-01-09 20:24:13 +0800

    倒序

---
+++ demoBoot/src/main/java/com/listTest/Demo7.java
@@ -0,0 +1,17 @@
+package com.listTest;
+
+public class Demo7 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        int length = arr.length;
+        int i1 = length - 1;
+        for (int i = length - 1; i >=0 ; i--) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("---");
+        for (int i = 0; i <length ; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println(arr);
+    }
+}
