2d1a16364362482dd01b0b0b693a8e3e17ce50cd
ming.li <1430793456@qq.com>
2024-02-05 11:13:31 +0800

    是否包含汉字或字母

--- testBoot/src/main/java/com/regexTest/Demo1.java
+++ testBoot/src/main/java/com/regexTest/Demo1.java
@@ -1,26 +1,31 @@
 package com.regexTest;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Demo1 {
     public static void main(String[] args) {
         String regex = "[\\u4e00-\\u9fa5a-zA-Z]+";
-        String s="111.";
+        String s="5mg*20片";
         System.out.println(checkContainsChineseOrLetter(s));
+        System.out.println(s.matches(".*[\\u4e00-\\u9fa5]+.*"));
     }
 
     private static boolean checkContainsChineseOrLetter(String inputStr) {
-        if (inputStr == null || inputStr.isEmpty()) {
-            return false;
-        } else {
-            // 使用正则表达式进行匹配
-            String regex1 = "[\\u4e00-\\u9fa5]+.*";
-            Pattern compile1 = Pattern.compile(regex1);
-            boolean matches1 = compile1.matcher(inputStr).matches();
-            String regex2 = "[a-zA-Z]+.*";
-            Pattern compile2 = Pattern.compile(regex2);
-            boolean matches2 = compile2.matcher(inputStr).matches();
-            return matches1||matches2;
+        String regex = ".*[a-zA-Z]+.*"; // 包含字母的正则表达式
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(inputStr);
+
+        boolean containsLetter = matcher.matches();
+
+        if (containsLetter) {
+            regex = ".*[\u4e00-\u9fa5]+.*"; // 包含中文的正则表达式
+            pattern = Pattern.compile(regex);
+            matcher = pattern.matcher(inputStr);
+            boolean containsChinese = matcher.matches();
+
+            return containsLetter && containsChinese;
         }
+        return false;
     }
 }
